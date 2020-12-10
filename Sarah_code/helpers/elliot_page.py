0e989b9157a9066499ce12af2b3b82b419a718ed
Sarah Stafford-Langan <sastaffo@tcd.ie>
2020-12-10 23:12:14 +0000

    updated elliot-page fixes and merging user-film data method

--- Sarah_code/helpers/elliot_page.py
+++ Sarah_code/helpers/elliot_page.py
@@ -1,73 +1,160 @@
-## replace all instances of "ellen-page" with "elliot-page" in ALL files
-## writes to csv if both names occur
-def file_replace_elliot_page(user_type="pop"):
-	fi_path = film_info_path.replace(".json", "_new.json")
-	fi_s = read_f(film_info_path)
-	fi_s = fi_s.replace("ellen-page", "elliot-page")
-	print("film info string replaced", dt.now())
-	write_f(fi_path, fi_s)
-	print("film info written", dt.now())
+elliot_fixes = folder_path + "elliot_fixes_to_do.csv"
+## replace all instances of "ellen-page" with "elliot-page" in ALL files where only DN occurs
+## writes others to csv
 
-	uj_path = (folder_path + "TYPE_users_X00_X99_new.json").replace("TYPE", user_type)
-	u_start = dt.now()
-	u_prev = u_start
-	for i in range(39):
-		i_path = uj_path.replace("X", str(i))
-		i_s = read_f(i_path)
-		print("\n",i, "file read")
-		if (i_s.find("ellen-page")) == -1:
-			# if file contains neither, no action needed
-			# if file contains 'elliot-page' but not 'ellen-page', no action needed
-			print("    no replacement needed")
-			u_prev = dt.now()
-			continue
-		else:
-			if (i_s.find("elliot-page")) == -1:
-				# if file contains 'ellen-page' but not 'elliot-page', straight replace
-				i_s = i_s.replace("ellen-page", "elliot-page")
-				print("    straight replace done")
-				write_f(i_path, i_s)
-				print("    written in", (dt.now()-u_prev))
-				u_prev = dt.now()
-			else:
-				# if file contains both: add to csv to deal with later
-				elliot_fixes = folder_path + "elliot_fixes.csv"
-				s = i_path + "\n"
-				append_f(elliot_fixes, s)
-				print("    contains both: added to elliot_fixes.csv ", (dt.now()-u_prev))
-				u_prev = dt.now()
-			#end if
-		#end if
-	#end for
-	print("\nall user json files parsed in ", (dt.now()-u_start))
-	return
+def film_dict_replace_elliot_page():
+    fi_path = film_info_path.replace("data", "data/elliot").replace(".json", "_old.json")
+    fi_old = read_f(film_info_path)
+    fi_new = fi_old.replace("ellen-page", "elliot-page")
+    print("film info string replaced")
+    write_f(fi_path, fi_old)
+    write_f(film_info_path, fi_new)
+    print("film info written\n")
+    return;
+
+def file_replace_elliot_page(ulist, user_type="pop"):
+    uj_path = (folder_path + "safety/TYPE_users_X00_X99.json").replace("TYPE", user_type)
+    u_start = dt.now()
+    print(user_type, ":", u_start)
+    for i in ulist:
+        i_path = uj_path.replace("X", str(i))
+        i_s = read_f(i_path)
+        if i_s is None: continue
+
+        pad = ""
+        if i<10: pad = "  "
+        print(i_path.split("/")[-1], pad, ". read", end=" . ")
+        yes = i_s.count("elliot-page")
+        no = i_s.count("ellen-page")
+        print("yes=", yes, ". no =", no, end=" . ")
+        if (no) == 0:
+            # if file contains neither, no action needed
+            # if file contains 'elliot-page' but not 'ellen-page', no action needed
+            print("skip")
+            continue
+        else:
+            if (yes) == 0:
+                # if file contains 'ellen-page' but not 'elliot-page', straight replace
+                i_s = i_s.replace("ellen-page", "elliot-page")
+                print("replaced", end=" . ")
+                write_f(i_path, i_s)
+                print("written")
+                u_prev = dt.now()
+            else:
+                # if file contains both: add to csv to deal with later
+                s = i_path + "\n"
+                append_f(elliot_fixes, s)
+                print("added to csv")
+                u_prev = dt.now()
+            #end if
+        #end if
+    #end for
+    print("all", user_type, "parsed in", (dt.now()-u_start))
+    return
 #end
 
 
-## replace all instances in the files where both names exist
+## fix names inside files
 def apply_elliot_fixes():
-	csv_path = folder_path + "elliot_fixes.csv"
-	file_paths = read_f(csv_path).split("\n")
-	for path in file_paths:
-		print("file: ", path.split("/")[-1] )
-		users_dict = json.loads(read_f(path))
-		for i in range(len(users_dict["users"])):
-			print("  ",i, end=":\n")
-			u = users_dict["users"][i]
-			ellen_avg = {}
-			elliot_avg = {}
-			try:
-				ellen_avg = u["average_rating_by_actor"]["ellen-page"]
-			except KeyError:
-				# user does not have ellen-page as an actor
-				print()
-				continue # to next user
-				try:
-					elliot_avg = u["average_rating_by_actor"]["elliot-page"]
-				except KeyError:
-					# user does not have elliot-page but *does* have ellen-page
-					users_dict["users"][i]["average_rating_by_actor"]["elliot-page"] = ellen_avg
-					continue # to next user
-					# user has both elliot-page and ellen-page
-					# todo: merge
-					#
+    csv_path = folder_path + "elliot_fixes_to_do.csv"
+    file_paths = read_f(csv_path).split("\n")
+    for path in file_paths:
+        read_in = read_f(path)
+        if read_in is None: continue
+
+        print("file:", path.split("/")[-1])
+        yes = read_in.count("elliot-page")
+        no = read_in.count("ellen-page")
+        print("  yes=", yes)
+        print("  no =", no)
+        users_dict = json.loads(read_in)
+        for i in range(len(users_dict["users"])):
+            u = users_dict["users"][i]
+            ellen_avg = {}
+            elliot_avg = {}
+            u_s = json.dumps(u)
+            yes = u_s.count("elliot-page")
+            no = u_s.count("ellen-page")
+            if (no == 0): ## DN is not present
+                continue
+            else:
+                pad = ""
+                if i<10: pad = "  "
+                print(" ",i, pad, "yes=", yes," no =", no, end=" ")
+                ellen_avg = u["average_rating_by_actor"]["ellen-page"]
+                if (yes == 0): ## DN is present, elliot is not
+                    u["average_rating_by_actor"]["elliot-page"] = ellen_avg # creates elliot
+                    u["average_rating_by_actor"].pop("ellen-page") # deletes ellen
+                    print("    replacing")
+                else: ## both names are present, *merge*
+                    print("    merging")
+                    elliot_avg = u["average_rating_by_actor"]["elliot-page"]
+                    tmplist = elliot_avg["film-lids"]
+                    for lid in ellen_avg["film-lids"]:
+                        tmplist.append(lid)
+                    # all films added to lids
+                    sum = 0
+                    count = len(tmplist)
+                    for lid in tmplist:
+                        sum = sum + u["ratings_by_film"][lid]
+                    # sum of all new_page film ratings
+                    avg = sum/count
+                    new_page = {
+                        "avg" : (avg),
+                        "count" : count,
+                        "avg_minus_tot_avg" : (avg-u["average_rating"]),
+                        "film-lids" : tmplist
+                    }
+                    # filled new dict
+                    u["average_rating_by_actor"]["elliot-page"] = new_page # replaces 'elliot-page'
+                    u["average_rating_by_actor"].pop("ellen-page") # deletes ellen
+                #end if yes
+            #end if no
+            users_dict["users"][i] = u # updates user in dict
+        #end for users
+        write_elliot(path.replace("safety", "elliot"), users_dict)
+    #end for paths
+    print("DONE")
+    return
+#end
+
+def write_elliot(path, out_dict):
+    write_f(path, "{ \"users\" : [\n")
+    first = True
+    for u in out_dict["users"]:
+        user_js = (json.dumps(u)) ## no formatting
+        user_js = user_js[0] + "\n" + user_js[1:] ## a little formatting, as a treat
+        if first: first = False
+        else: user_js = (",\n") + user_js
+        append_f(path, user_js)
+    #end for
+    append_f(path, "\n] }")
+    print("  written")
+
+    check = read_f(path)
+    yes = check.count("elliot-page")
+    no = check.count("ellen-page")
+    print("  new yes=", yes)
+    print("  new no =", no)
+    return
+
+
+gen_file = "safety/TYPE_users_X00_X99.json"
+def elliot_checkfix_all_files():
+    write_f(elliot_fixes, "")
+    print("clearing csv\n")
+
+    ns = []
+    for n in range(39):
+        ns.append(n)
+    file_replace_elliot_page(ns, user_type="pop")
+    print()
+
+    for n in range(39,58):
+        ns.append(n)
+    file_replace_elliot_page(ns, user_type="gen")
+    print()
+
+    apply_elliot_fixes()
+    return
+#end
--- Sarah_code/merge_user_film.py
+++ Sarah_code/merge_user_film.py
@@ -1,48 +1,96 @@
-## film has: [genres] dir_name [actors]
+folder_path = "/content/drive/MyDrive/4th_Year/ML Group Project/Sarah_data/safety/"
+
+## uses old unmerged film data from Philip's Data/all_filmsV2.json 10/Dec/2020 8pm
+
+"""
+{  "34722": {
+    "name": "Inception",
+    "url": "/film/inception/",
+    "lid": "34722",
+    "tmdb_id": "27205",
+    "number_of_ratings": 536165,
+    "avg_rating": 4.18,
+    "genres": [
+      "Science Fiction",
+      "Action",
+      "Adventure"
+    ],
+    "director_url": "/director/christopher-nolan/",
+    "actors_urls": [
+      "/actor/leonardo-dicaprio/",
+      "/actor/ken-watanabe/",
+      "/actor/joseph-gordon-levitt/",
+      "/actor/marion-cotillard/",
+      "/actor/elliot-page/"
+    ],
+    "number_of_likes": 353380,
+    "number_of_views": 926820
+  }}
+"""
 
 def get_relevant_fields(user_json, film_json):
-	relevant = {}
-	try:
-		dir = film_json["director"]
-		u_dir = user_json["average_rating_by_director"][dir]
-		relevant[dir] =
-		for g in film_json["genres"]:
-			relevant[g] = user_json["average_rating_by_genre"][g]
-	except KeyError: # user hasn't rated anything from this director/genre/actor : this is fine
-		pass # will 'pass' return to where the
-	except Exception as e: # this is not fine
-		print("Something happened: ", e)
-		print(traceback.format_exc())
-		raise
+    relevant = {}
+    relevant["user_lid"] = user_json["lid"]
+    relevant["film_lid"] = film_json["lid"]
+    relevant["user_country"] = user_json["country"]
+    relevant["user_films_watched"] = len(user_json["ratings_by_film"])
+    relevant["film_total_watches"] = film_json["number_of_views"]
+    relevant["film_total_likes"] = film_json["number_of_likes"]
+    relevant["director"] = try_json(user_json, film_json, "director")
+    for i in range(len(film_json["genres"])):
+        key = film_json["genres"][i]
+        relevant[key] = try_json(user_json, film_json, "genre", index=i)
+    for i in range(5):
+        key = "actor"+str(i)
+        relevant[key] = try_json(user_json, film_json, "actor", index=i)
+    print(json.dumps(relevant, indent=2))
+    return relevant
+#end
+
+def try_json(user_json, film_json, field, index=0):
+    if field == "director":
+        x = film_json[(field+"_url")]
+        x = x.split("/")[2]
+    elif field == "actor":
+        x = film_json[(field+"s_urls")][index]
+        x = x.split("/")[2]
+    elif field == "genre":
+        #print(film_json)
+        x = film_json[(field+"s")][index]
+    else: return None
+    print(x, end=" : ")
+    #end if
+    try:
+        u_x_avg = user_json[("average_rating_by_"+field)][x]["avg_minus_tot_avg"]
+        print("got x")
+        return u_x_avg
+    except KeyError as ke:
+        print("no x", ke)
+        return None
+    except Exception as e:
+        print("Something happened: ", e)
+        print(traceback.format_exc())
+    #end try
+#end
 
 def test_get_revelant_fields():
-	film_s = """{
-		"name": "Inception",
-		"lid": "34722",
-		"tmdb_id": "27205",
-		"number_of_ratings": 533369,
-		"avg_rating": 4.18,
-		"genres": [
-			"Science Fiction",
-			"Action",
-			"Adventure"
-		],
-		"director": [
-			"christopher-nolan"
-		],
-		"actor": [
-			"leonardo-dicaprio",
-			"ken-watanabe",
-			"joseph-gordon-levitt",
-			"marion-cotillard",
-			"elliot-page"
-		]
-	}""" # more info obviously added later
-	film_dict = json.loads((film_s.lower())) # make sure everything is lower case
-	# user who has watched Inception: "deathproof" from 000_099.json
-	all = json.loads( read_f((folder_path+"pop_users_000_099.json"))
-	for u in all["users"]:
-		if u["lid"] == "deathproof":
-			#
-	#end for
-	return
+    inception_lid = "34722"
+    film_data_path = folder_path.replace("Sarah_data/safety/", "Philip's Data/all_filmsV2.json")
+    all_films = json.loads(read_f(film_data_path).lower())
+    inception = all_films[inception_lid]
+    #print(json.dumps(inception,indent=2), "\n")
+
+    # user who has watched Inception: "captstevezissou" from 000_099.json
+    captstevezissou = {}
+    all = json.loads( read_f((folder_path+"gen_users_5100_5199.json")) )
+    for u in all["users"]:
+        if u["lid"] == "captstevezissou":
+            captstevezissou = u
+            break
+    #end for
+    #print(json.dumps(captstevezissou, indent=2), "\n")
+    get_relevant_fields(captstevezissou, inception)
+    return
+
+print(dt.now())
+#test_get_revelant_fields()

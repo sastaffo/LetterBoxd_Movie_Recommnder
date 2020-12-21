b2ca8290465a0a1a52e2ce390d234e14b658f5df
ShaunJose <shaun.jose12@gmail.com>
2020-12-21 22:17:46 +0000

    Implemented collaborative filtering + update readme

--- collab_filtering/README.md
+++ collab_filtering/README.md
@@ -1,11 +1,12 @@
-# Description   
+# Item-based Collaborative Filtering    
 
-Item-based Collaborative Filtering   
+### Description   
 
 ```filter_data.py``` - Takes all files data from a folder 'user_film_merge' and creates a refined list of these files (only the data needed for item-based collaboraitve filtering)   
 
 ```collab_filtering``` - Use the refined data file created by the code file above and performs collaborative filtering based on that data
 
+        
 ### Run     
 
 1. Creating a refined list first:     
--- collab_filtering/collab_filtering.py
+++ collab_filtering/collab_filtering.py
@@ -1,7 +1,10 @@
-from lenskit.datasets import ML100K
-from lenskit import batch, topn, util
+"""
+@author: Shaun
+"""
+
+from lenskit import batch, util
 from lenskit import crossfold as xf
-from lenskit.algorithms import Recommender, als, item_knn as knn
+from lenskit.algorithms import Recommender, item_knn as knn
 from lenskit.metrics.predict import rmse
 import json
 import os
@@ -10,6 +13,9 @@ import pandas as pd
 algo_ii = knn.ItemItem(20)
 
 def eval(aname, algo, train, test, all_preds):
+    """
+    Predict the raitings using the model
+    """
     fittable = util.clone(algo)
     fittable = Recommender.adapt(fittable)
     fittable.fit(train)
@@ -19,6 +25,9 @@ def eval(aname, algo, train, test, all_preds):
     all_preds.append(preds)
 
 def main():
+    """
+    Perform item based collaborative filtering from the ratings data in 'ratings' variable
+    """
     all_preds = []
     test_data = []
     for train, test in xf.partition_users(ratings[['user', 'item', 'rating']], 5, xf.SampleFrac(0.2)):
@@ -30,34 +39,26 @@ def main():
     test_data = pd.concat(test_data, ignore_index = True)
     print('RMSE II:', rmse(preds_ii['prediction'],preds_ii['rating']))
 
+
 if __name__ == '__main__':
 
-    # get all files in directory
-    dir = "../user_film_merge/"
-    user_film_files = os.listdir(dir)
-
-    # get data from all files into one big list
-    all_data = []
-    for filename in user_film_files:
-        filepath = dir + filename
-        with open(filepath) as fp:
-            files_data = json.load(fp)
-            all_data.extend(files_data)
-
-    # get all the users, films and ratings for the films by the users from the list
+    filepath = "refined_film_data.json"
     user_lids = []
     film_lids = []
     users_ratings = []
-    for user_film_pair in all_data:
-        user_lids.append(user_film_pair["user_lid"])
-        film_lids.append(user_film_pair["film_lid"])
-        users_ratings.append(user_film_pair["user_rating_for_film"])
+
+    # Read user id's, film id's and user's ratings of films into different lists, from the refined file given
+    with open(filepath) as fp:
+        all_users_films = json.load(fp)
+        for user_film_pair in all_users_films:
+            user_lids.append(user_film_pair["user_lid"])
+            film_lids.append(user_film_pair["film_lid"])
+            users_ratings.append(user_film_pair["user_rating_for_film"])
 
     # add user ids, films ids and ratings to the pandas dataframe
     ratings = pd.DataFrame({'user' : user_lids,
                             'item' : film_lids,
                             'rating' : users_ratings})
-    print(ratings.head())
 
-    # collaborative filtering
-    # main()
+    # Perform collaborative filtering on data in ratings
+    main()
